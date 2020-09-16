d6ff8ce65304a04a2d3b0ea859dc7db035769cc1
Alikvk <ali.kavak1905@gmail.com>
2020-09-17 00:49:52 +0300

    test2

--- lib/frontend/scaffold.dart
+++ lib/frontend/scaffold.dart
@@ -9,19 +9,100 @@ class MyScaffoldWidget extends StatelessWidget {
         title: const Text('AppBar Demo'),
         actions: <Widget>[
           IconButton(
-            icon: const Icon(Icons.add_alert),
-            tooltip: 'Show Snackbar',
-            onPressed: () =>
-                print('Show Snackbar'), // butona tıklandığında bu cümleyi yaz
-          ),
+              icon: const Icon(Icons.add_alert),
+              tooltip: 'firstpage',
+              onPressed: () => Navigator.pushNamed(context, '/firstpage')),
           IconButton(
-            icon: const Icon(Icons.navigate_next),
-            tooltip: 'Next page',
-            onPressed: () => print('Next Page'),
+              icon: const Icon(Icons.navigate_before),
+              tooltip: 'statelessstatefullwidget',
+              onPressed: () =>
+                  Navigator.pushNamed(context, '/statelessstatefullwidget')),
+          IconButton(
+              icon: const Icon(Icons.navigate_next),
+              tooltip: 'hello',
+              onPressed: () => Navigator.pushNamed(context, '/firstpage')),
+        ],
+      ),
+      body: GridView.count(
+        primary: false,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 3,
+        children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(8),
+            child: const Text("He'd have you all unravel at the"),
+            color: Colors.teal[100],
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(8),
+            child: const Text('Heed not the rabble'),
+            color: Colors.teal[200],
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(8),
+            child: const Text('Sound of screams but the'),
+            color: Colors.teal[300],
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(8),
+            child: const Text('Who scream'),
+            color: Colors.teal[400],
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(8),
+            child: const Text('Revolution is coming...'),
+            color: Colors.teal[500],
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(8),
+            child: const Text('Revolution, they...'),
+            color: Colors.teal[600],
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(8),
+            child: const Text("He'd have you all unravel at the"),
+            color: Colors.teal[100],
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(8),
+            child: const Text('Heed not the rabble'),
+            color: Colors.teal[200],
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(8),
+            child: const Text('Sound of screams but the'),
+            color: Colors.teal[300],
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(8),
+            child: const Text('Who scream'),
+            color: Colors.teal[400],
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(8),
+            child: const Text('Revolution is coming...'),
+            color: Colors.teal[500],
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(8),
+            child: const Text('Revolution, they...'),
+            color: Colors.teal[600],
           ),
         ],
       ),
-      body: const Center(child: Text('Dendassta')),
       bottomNavigationBar: new BottomNavigationBar(
           // Alt barı oluşturduğumuz kısım
           items: [
